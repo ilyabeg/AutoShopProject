00394cf859b9d2401f0e9e98c1249198ef04149a
Ilya Beg <ilyabegichev@gmail.com>
2026-07-06 15:51:04 +0300

    feat: Add Commands UI for manager and customer

--- AutoShopProject/Application/Manager.cs
+++ AutoShopProject/Application/Manager.cs
@@ -6,5 +6,33 @@ namespace AutoShopProject.Application
 {
     internal class Manager
     {
+        private readonly string _secretPassword = "123";
+        private ManagerCommandsUI cmd;
+
+        public void Login()
+        {
+            Console.WriteLine("[MANAGER] Enter secret password: ");
+            string input = Console.ReadLine().Trim().ToUpper();
+
+            if (!ValidPassword(input))
+            {
+                do
+                {
+                    Console.WriteLine("[MANAGER] Invalid input. Try again: ");
+                    input = Console.ReadLine().Trim().ToUpper();
+                }
+                while (!ValidPassword(input));
+            }
+
+            if (input == "EXIT") return;
+
+            cmd = new ManagerCommandsUI();
+            cmd.Run();
+        }
+
+        private bool ValidPassword(string input)
+        {
+            return input == _secretPassword && input != "EXIT";
+        }
     }
 }
---
+++ AutoShopProject/Command Managers/CustomerCommandsUI.cs
@@ -0,0 +1,11 @@
+﻿using AutoShopProject.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutoShopProject.Command_Managers
+{
+    internal class CustomerCommandsUI : ICommandUser
+    {
+    }
+}
