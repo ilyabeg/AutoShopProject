d25c09d5b741efe0e5adab02caaaa063d181bfb3
Ilya Beg <ilyabegichev@gmail.com>
2026-07-06 15:37:43 +0300

    feat: Add Manager and Customer clesses

--- AutoShopProject/Application/AutoShop.cs
+++ AutoShopProject/Application/AutoShop.cs
@@ -6,9 +6,42 @@ namespace AutoShopProject.Application
 {
     internal class AutoShop
     {
+        private Catalog _catalog;
+        private Manager _manager;
+        private Customer _cutomer;
+
         public void Run()
         {
+            _catalog = new Catalog();
 
+            Console.WriteLine("Welcome to the Auto Shop! Please register as 'M' (MANAGER) or 'C' (Customer) >>");
+            char input = Console.ReadKey().KeyChar;
+
+            if (!IsValid(input))
+            {
+                do
+                {
+                    Console.WriteLine("Invalid input! Please register as 'M' (MANAGER) or 'C' (Customer) >>");
+                    input = Console.ReadKey().KeyChar;
+                }
+                while (!IsValid(input));
+            }
+
+            if (input == 'M' || input == 'm')
+            {
+                _manager.Login();
+            }
+            else
+            {
+
+            }
+
+
+        }
+
+        private bool IsValid(char input)
+        {
+            return input == 'M' || input == 'C' || input == 'm' || input == 'c';
         }
     }
 }
