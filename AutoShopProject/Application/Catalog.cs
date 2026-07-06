e1b22fd69599fd51e3f1b9ac2a02abc1bd25e352
Ilya Beg <ilyabegichev@gmail.com>
2026-07-06 15:14:39 +0300

    feat: Add show car catalog and engines methods to Catalog class

--- AutoShopProject/Application/Catalog.cs
+++ AutoShopProject/Application/Catalog.cs
@@ -9,14 +9,48 @@ namespace AutoShopProject.Application
     internal class Catalog
     {
         // car catalog list from json file
-        private readonly List<Car>? _catalog = JsonSerializer.Deserialize<List<Car>>(File.ReadAllText("car_catalog.json"));
+        private List<Car>? _catalog = JsonSerializer.Deserialize<List<Car>>(File.ReadAllText("car_catalog.json"));
 
         // engines from json file
         private List<Engine>? _engines = JsonSerializer.Deserialize<List<Engine>>(File.ReadAllText("engines.json"));
 
         public void ShowCarCatalog()
-        { 
+        {
+            Console.WriteLine("===========================================================");
+            Console.WriteLine("                     Car Catalog:");
+            Console.WriteLine("===========================================================");
+            Console.WriteLine();
 
+            foreach (Car car in _catalog)
+            {
+                Console.WriteLine("\tCar Type:"          + $"\t{car.CarType}.");
+                Console.WriteLine("\tCar Manufacturer:"  + $"\t{car.Manufacturer}.");
+                Console.WriteLine("\tCar Model:"         + $"\t{car.Model}.");
+                Console.WriteLine("\tCar Engine ID:"     + $"\t{car.EngineID}.");
+                Console.WriteLine("\tCar Year:"          + $"\t{car.Year}.");
+                Console.WriteLine("\tCar Drivetrain:"    + $"\t{car.Drivetrain}.");
+                Console.WriteLine("\tCar Seat number:"   + $"\t{car.Seats}.");
+                Console.WriteLine("\tCar Door number:"   + $"\t{car.Doors}.");
+                Console.WriteLine("\tCar Price:"         + $"\t{car.Price}$.");
+                Console.WriteLine();
+            }
+        }
+
+        public void ShowEngineCatalog() 
+        {
+            Console.WriteLine("===========================================================");
+            Console.WriteLine("                     Engine Catalog:");
+            Console.WriteLine("===========================================================");
+            Console.WriteLine();
+
+            foreach (Engine engine in _engines)
+            {
+                Console.WriteLine("\tEngine ID:"          + $"\t{engine.id}");
+                Console.WriteLine("\tEngine Type:"        + $"\t{engine.Type}");
+                Console.WriteLine("\tEngine Volume:"      + $"\t{engine.Volume}");
+                Console.WriteLine("\tEngine Horsepower:"  + $"\t{engine.Horsepower}");
+                Console.WriteLine();
+            }
         }
     }
 }
